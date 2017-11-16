59326b8fca531c4c29a7ae4ea41efa2ef085bc66
irina.visotskaya <i.halubtsova@besk.com>
2017-11-16 14:34:15 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -14,34 +14,66 @@ $(document).ready(function(){
 
     var entries = [
 
-        { label: 'Dev Blog', url: 'http://niklasknaack.blogspot.de/', target: '_top' },
-        { label: 'Flashforum', url: 'http://www.flashforum.de/', target: '_top' },
-        { label: 'jQueryScript.net', url: 'http://www.jqueryscript.net/', target: '_top' },
-        { label: 'Javascript-Forum', url: 'http://forum.jswelt.de/', target: '_top' },
-        { label: 'JSFiddle', url: 'https://jsfiddle.net/user/NiklasKnaack/fiddles/', target: '_top' },
-        { label: 'CodePen', url: 'http://codepen.io/', target: '_top' },
-        { label: 'three.js', url: 'http://threejs.org/', target: '_top' },
-        { label: 'WebGLStudio.js', url: 'http://webglstudio.org/', target: '_top' },
-        { label: 'JS Compress', url: 'http://jscompress.com/', target: '_top' },
-        { label: 'TinyPNG', url: 'https://tinypng.com/', target: '_top' },
-        { label: 'Can I Use', url: 'http://caniuse.com/', target: '_top' },
-        { label: 'URL shortener', url: 'https://goo.gl/', target: '_top' },
-        { label: 'HTML Encoder', url: 'http://www.opinionatedgeek.com/DotNet/Tools/HTMLEncode/Encode.aspx', target: '_top' },
-        { label: 'Twitter', url: 'https://twitter.com/niklaswebdev', target: '_top' },
-        { label: 'deviantART', url: 'http://nkunited.deviantart.com/', target: '_top' },
-        { label: 'Gulp', url: 'http://gulpjs.com/', target: '_top' },
-        { label: 'Browsersync', url: 'https://www.browsersync.io/', target: '_top' },
-        { label: 'GitHub', url: 'https://github.com/', target: '_top' },
-        { label: 'Shadertoy', url: 'https://www.shadertoy.com/', target: '_top' },
-        { label: 'Starling', url: 'http://gamua.com/starling/', target: '_top' },
-        { label: 'jsPerf', url: 'http://jsperf.com/', target: '_top' },
-        { label: 'Foundation', url: 'http://foundation.zurb.com/', target: '_top' },
-        { label: 'CreateJS', url: 'http://createjs.com/', target: '_top' },
-        { label: 'Velocity.js', url: 'http://julian.com/research/velocity/', target: '_top' },
-        { label: 'TweenLite', url: 'https://greensock.com/docs/#/HTML5/GSAP/TweenLite/', target: '_top' },
-        { label: 'jQuery', url: 'https://jquery.com/', target: '_top' },
-        { label: 'jQuery Rain', url: 'http://www.jqueryrain.com/', target: '_top' },
-        { label: 'jQuery Plugins', url: 'http://jquery-plugins.net/', target: '_top' },
+        {label: 'documents', url: '#', target: '_top', tooltip: 'Lorem ipsum'},
+        {label: 'SUPPLIERS', url: '#', target: '_top', tooltip: 'Dolor sit amet'},
+        {label: 'cities', url: '#', target: '_top', tooltip: 'Consetetur sadipscing'},
+        {label: 'Last', url: '#', target: '_top', tooltip: 'Sed diam'},
+        {label: 'minute', url: '#', target: '_top'},
+        {label: 'offer', url: '#', target: '_top', tooltip: 'At vero'},
+        {label: 'users', url: '#', target: '_top', tooltip: 'Nonumy eirmod'},
+        {label: 'deals', url: '#', target: '_top', tooltip: 'Stet clita'},
+        {label: 'search', url: '#', target: '_top', tooltip: 'Justo duo'},
+        {label: 'engine', url: '#', target: '_top', tooltip: 'Ut wisi enim'},
+        {label: 'Ratings', url: '#', target: '_top', tooltip: 'Minim veniam'},
+        {label: 'favorites', url: '#', target: '_top', tooltip: 'Quis nostrud'},
+        {label: 'contact', url: '#', target: '_top'},
+        {label: 'details', url: '#', target: '_top', tooltip: 'Veniam isictus'},
+        {label: 'phone', url: '#', target: '_top', tooltip: 'Autem insto'},
+        {label: 'guides', url: '#', target: '_top', tooltip: 'Officia dolor'},
+        {label: 'booking', url: '#', target: '_top', tooltip: 'Digi tal'},
+        {label: 'friends', url: '#', target: '_top', tooltip: 'Amet et quam'},
+        {label: 'likes', url: '#', target: '_top', tooltip: 'Meno equox'},
+        {label: 'Routes', url: '#', target: '_top', tooltip: 'Duis autem'},
+        {label: 'official', url: '#', target: '_top', tooltip: 'Soluta nobis'},
+        {label: 'attractions', url: '#', target: '_top', tooltip: 'Blandit praesent'},
+        {label: 'countries', url: '#', target: '_top', tooltip: 'Dignissim qui'},
+        {label: 'customs', url: '#', target: '_top', tooltip: 'Et iusto odio'},
+        {label: 'realities',url: '#',target: '_top',tooltip: 'Facilisis at vero'},
+        {label: 'social', url: '#', target: '_top', tooltip: 'Dolore eu'},
+        {label: 'interests', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'hotels', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'offers', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'cards', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'airports', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'air tickets', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'check in visa', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'connections', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'categories', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'features', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'History', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'subscriptions', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'Wishlist', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'events', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'video', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'attractions', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'apartments', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'suppliers', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'profiles', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'accommodation', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'bookmarks', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'reviews', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'tours', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'comments', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'email', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'services', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'requests', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'moving', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'weather', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'shopping', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'prices', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'information', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'ratings data', url: '#', target: '_top', tooltip: 'In vulputate'},
+        {label: 'guidebooks', url: '#', target: '_top', tooltip: 'In vulputate'}
 
     ];
 
@@ -58,7 +90,7 @@ $(document).ready(function(){
         opacityOut: 0.1,
         opacitySpeed: 6,
         fov: 800,
-        speed: 0.5,
+        speed: 0.2,
         fontFamily: 'GothamProMedium, Arial, sans-serif',
         fontSize: '16',
         fontColor: '#3dbdf4',
@@ -75,7 +107,7 @@ $(document).ready(function(){
         tooltipFontToUpperCase: false,
         tooltipTextAnchor: 'left',
         tooltipDiffX: 0,
-        tooltipDiffY: 10
+        tooltipDiffY: 20
 
     };
 
@@ -112,12 +144,58 @@ $(document).ready(function(){
         }
     });
 
-    $("#getting-started")
-        .countdown('2017/11/30', function(event) {
+
+    $(".getting-started")
+        .countdown('2017/11/30 14:00:00', function(event) {
             var $this = $(this).html(event.strftime(''
-                + '<span class="number">%d</span> : '
+                + '<span class="number">%D</span> : '
                 + '<span class="number">%H</span> : '
                 + '<span class="number">%M</span> : '
                 + '<span class="number">%S</span>'));
         });
+
+    $('.slide-lead').slick({
+        arrows:true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    $('.slide-activity').slick({
+        arrows:true,
+        slidesToShow: 2,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
